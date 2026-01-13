0938144069ad891f23d47044b1685275283c74d2
elissar421 <elesarhayan@gmail.com>
2026-01-13 18:03:02 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+  var input = document.getElementById("taskInput");
+  var task = input.value;
+
+  if (task === "") return;
+
+  var li = document.createElement("li");
+  li.textContent = task;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
